2f8cdfec11e33f1bf47246201209a35b49d41903
Potter, Timothy <Tim.Potter@viasat.com>
2023-10-28 13:30:48 +1100

    Check in template of HTTP game server

---
+++ server/index.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Connect-4 server listening on port ${port}`)
+})
