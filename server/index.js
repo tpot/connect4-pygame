bb70f11acaeb888ce9038489dc108e05bad96ec3
Potter, Timothy <Tim.Potter@viasat.com>
2023-10-29 09:09:57 +1100

    Start working on updating game from posted moves

--- server/index.js
+++ server/index.js
@@ -33,12 +33,24 @@ const validateGameID = (req, res, next) => {
     }
     next()
 }
-  
+
+function newGameState() {
+    return {
+	turn: 1,
+	winner: 0,
+	board: [[0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0]]}
+}
+
 // Create a new game, return game ID
 
 app.post('/game', (req, res) => {
     const id = gameIdCounter++
-    state[id] = {}
+    state[id] = newGameState()
     res.json({id})
 })
 
@@ -53,11 +65,89 @@ app.get(
 )
 
 // Update game status
-	
+
+
+function updateBoard(board, player, column) {
+    const new_board = JSON.parse(JSON.stringify(board))
+    for(row = 5; row >= 0; row--) {
+	if (board[row][column] === 0) {
+	    new_board[row][column] = player
+	    return new_board
+	}
+    }
+    throw new Error('Invalid move')
+}
+
+function validMove(board, player, column) {
+    // Column is already full
+    if (board[0][column] != 0) {
+	return false
+    }
+    
+    return true
+}
+
+function updateWinner(board) {
+    return 0
+}
+
 app.put(
     '/game/:gameId',
     validateGameID,
     (req, res) => {
-	res.json({})
+	const s = state[req.params.gameId]
+
+	const player = parseInt(req.query.player)
+	const column = parseInt(req.query.column)
+
+	// Check whether it's the players turn
+	if (s.turn != player) {
+	    res.status(400).json({
+		message: `Invalid player ${player}`,
+	    })
+	    return
+	}
+
+	// Check valid column
+	if (column < 0 || column > 7) {
+	    res.status(400).json({ 
+		message: `Invalid column ${column}`,
+	    })
+	    return
+	}
+
+	// Check for valid move
+	if (!validMove(s.board, player, column)) {
+	    res.status(400).json({
+		message: `Invalid move`,
+	    })
+	    return
+	}
+
+	// Update board with token
+	s.board = updateBoard(s.board, player, column)
+
+	// Check for winner
+	s.winner = updateWinner(s.board)
+
+	// Switch player
+	if (player == 1) {
+	    s.turn = 2
+	} else {
+	    s.turn = 1
+	}
+
+	res.json(s)
     }
 )
+
+// Player's key string = 4 digits randomly generated with lowercase letters and 0-9. There are 1,679,616 different combinations, which is secure enough.
+
+/*
+
+deadline
+
+---------------------------- x ------ x ---------------------------
+                               U
+
+*/
